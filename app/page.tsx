ab6132882d8fbfb2ef6665cadcff736c83456d19
BORI <88658551+Kbomi16@users.noreply.github.com>
2024-11-27 15:15:56 +0900

    ✨ feat: 랜딩페이지 기초 구현 (#2)
    * 💄 style: 폰트 적용
    
    * ✨ feat: 랜딩페이지 기초 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,47 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      start
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b px-4 text-center">
+      <motion.div
+        className="mt-10"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ delay: 1, duration: 0.5 }}
+      >
+        <Image
+          src="/imgs/logo.png"
+          alt="Habit Tracker Illustration"
+          width={400}
+          height={300}
+          priority
+        />
+      </motion.div>
+      <motion.p
+        className="mt-4 text-lg text-slate-700"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 1 }}
+      >
+        당신의 성장을 돕는 스마트한 방법, GrowBit에서 시작하세요.
+      </motion.p>
+
+      <motion.div
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 1.5, duration: 1 }}
+        className="mt-8"
+      >
+        <Link href="/login">
+          <button className="hover:bg-green-70 bg-green-60 rounded-full px-6 py-3 text-lg font-semibold text-white transition">
+            Get Started
+          </button>
+        </Link>
+      </motion.div>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
-export default {
+const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        bold: ['AppleSDGothicNeoB'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        green: {
+          10: '#E3F9E5',
+          20: '#C1F2C1',
+          30: '#A0EAA0',
+          40: '#81C645',
+          50: '#6AA83A',
+          60: '#4D8C2F',
+          70: '#3A7225',
+          80: '#2E5C1F',
+          90: '#1E4A19',
+          100: '#1D392B',
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+}
+export default config
