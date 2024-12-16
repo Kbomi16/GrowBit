b6c7440b925fcd0001908f573f95cb566fe795a7
BORI <88658551+Kbomi16@users.noreply.github.com>
2024-12-16 13:05:51 +0900

    ✨ feat: 루틴 카드 페이지 구현 (#4)
    * 💄 style: 메인 UI 작업
    
    * ✨ feat: 습관 추가 모달
    
    * 📦 chore: 리액트 캘린더 추가
    
    * 💄 style: date-picker로 디자인 수정
    
    * ✨ feat: 리액트 캘린더 기초구현
    
    * ✨ feat: 루틴 캘린더 띄우기
    
    * 💄 style: 날짜 css 커스텀
    
    * ✨ feat: 루틴 삭제

--- app/(route)/(main)/main/page.tsx
+++ app/(route)/(main)/main/page.tsx
@@ -1,11 +1,177 @@
-import AuthGuard from '@/app/_components/AuthGuard'
+'use client'
+import AddHabitModal from '@/app/_components/modal/AddHabitModal'
+import { db } from '@/app/_utils/firebaseConfig'
+import {
+  collection,
+  getDocs,
+  updateDoc,
+  arrayUnion,
+  doc,
+  deleteDoc,
+} from 'firebase/firestore'
+import { useState, useEffect } from 'react'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
+import '@/public/styles/reactCalendar.css'
+import { FiTrash } from 'react-icons/fi'
+
+type Habit = {
+  id: string
+  name: string
+  startDate: string
+  endDate: string
+  frequency: string[]
+  completedDates: string[]
+}
 
 export default function Main() {
+  const [habits, setHabits] = useState<Habit[]>([])
+  const [showModal, setShowModal] = useState(false)
+
+  const fetchHabits = async () => {
+    const habitsCollection = collection(db, 'habits')
+    const habitSnapshot = await getDocs(habitsCollection)
+    const habitList = habitSnapshot.docs.map((doc) => {
+      const habitData = doc.data()
+      return {
+        id: doc.id,
+        name: habitData.name,
+        startDate: habitData.startDate,
+        endDate: habitData.endDate,
+        frequency: habitData.frequency,
+        completedDates: habitData.completedDates || [],
+      }
+    })
+    setHabits(habitList)
+  }
+
+  useEffect(() => {
+    fetchHabits()
+  }, [])
+
+  const handleDateClick = async (habitId: string, date: Date) => {
+    const dateString = date.toISOString().split('T')[0] // YYYY-MM-DD 형식
+    const habit = habits.find((h) => h.id === habitId)
+
+    if (habit && !habit.completedDates.includes(dateString)) {
+      const confirmCompletion = window.confirm(
+        `${habit.name}을(를) 완료하시겠습니까?`,
+      )
+      if (confirmCompletion) {
+        const habitRef = doc(db, 'habits', habitId)
+        await updateDoc(habitRef, {
+          completedDates: arrayUnion(dateString),
+        })
+        fetchHabits()
+      }
+    }
+  }
+
+  // 주어진 날짜(date)가 특정 습관(habit)에 대해 클릭 가능한 날짜인지 확인
+  const isDateClickable = (habit: Habit, date: Date) => {
+    const dayOfWeek = date.getDay()
+    const frequencyDays = habit.frequency.map((day) =>
+      ['일', '월', '화', '수', '목', '금', '토'].indexOf(day),
+    )
+
+    return (
+      frequencyDays.includes(dayOfWeek) &&
+      date >= new Date(habit.startDate) &&
+      date <= new Date(habit.endDate)
+    )
+  }
+
+  const isDateCompleted = (habit: Habit, date: Date) => {
+    const dateString = date.toISOString().split('T')[0]
+    return habit.completedDates.includes(dateString)
+  }
+
+  const isDateMissed = (habit: Habit, date: Date) => {
+    const today = new Date()
+    return (
+      date < today && // 오늘 이전 날짜
+      isDateClickable(habit, date) // 클릭 가능한 날짜인지 확인
+    )
+  }
+
+  const handleDeleteHabit = async (habitId: string) => {
+    const confirmDelete = window.confirm('이 루틴을 삭제하시겠습니까?')
+    if (confirmDelete) {
+      const habitRef = doc(db, 'habits', habitId)
+      await deleteDoc(habitRef)
+      fetchHabits()
+    }
+  }
+
   return (
-    <AuthGuard>
-      <div>
-        <h1>메인 페이지</h1>
+    <div className="mx-auto w-full max-w-[1200px]">
+      <div className="p-4">
+        <button
+          onClick={() => setShowModal(true)}
+          className="w-full rounded-full bg-green-30 px-6 py-3 text-white shadow-lg transition hover:bg-green-40"
+        >
+          루틴 추가
+        </button>
+
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2">
+          {habits.map((habit) => (
+            <div key={habit.id} className="rounded-lg bg-white p-6 shadow-md">
+              <div className="flex justify-between">
+                <h3 className="mb-2 text-xl font-semibold text-gray-800">
+                  {habit.name}
+                </h3>
+                <button
+                  onClick={() => handleDeleteHabit(habit.id)}
+                  className="text-red-500"
+                  aria-label="루틴 삭제 버튼"
+                >
+                  <FiTrash size={20} />
+                </button>
+              </div>
+              <p className="text-gray-600">시작 날짜: {habit.startDate}</p>
+              <p className="text-gray-600">종료 날짜: {habit.endDate}</p>
+              <p className="mb-4 text-gray-600">
+                매주 수행 요일: {habit.frequency.join(', ')}
+              </p>
+              <p className="font-semibold text-gray-800">
+                완료된 일수: {habit.completedDates.length}일
+              </p>
+
+              {/* 날짜 선택 캘린더 */}
+              <div className="mt-4">
+                <Calendar
+                  tileClassName={({ date }) => {
+                    if (isDateCompleted(habit, date)) {
+                      return 'completed'
+                    } else if (isDateMissed(habit, date)) {
+                      return 'missed'
+                    } else if (isDateClickable(habit, date)) {
+                      return 'clickable'
+                    } else {
+                      return 'not-clickable'
+                    }
+                  }}
+                  onClickDay={(date) => {
+                    if (
+                      isDateClickable(habit, date) &&
+                      !isDateMissed(habit, date)
+                    ) {
+                      handleDateClick(habit.id, date)
+                    }
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {showModal && (
+          <AddHabitModal
+            onClose={() => setShowModal(false)}
+            onAddHabit={fetchHabits}
+          />
+        )}
       </div>
-    </AuthGuard>
+    </div>
   )
 }
---
+++ app/_components/modal/AddHabitModal.tsx
@@ -0,0 +1,147 @@
+import { db } from '@/app/_utils/firebaseConfig'
+import { addDoc, collection } from 'firebase/firestore'
+import { ChangeEvent, useState } from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import '@/public/styles/reactDatePicker.css'
+
+type HabitModalProps = {
+  onClose: () => void
+  onAddHabit: () => void
+}
+
+type Habit = {
+  name: string
+  startDate: string
+  endDate: string
+  frequency: string[]
+  completedDates: string[]
+}
+
+export default function AddHabitModal({
+  onClose,
+  onAddHabit,
+}: HabitModalProps) {
+  const [habitName, setHabitName] = useState('')
+  const [startDate, setStartDate] = useState<Date | null>(null)
+  const [endDate, setEndDate] = useState<Date | null>(null)
+  const [frequency, setFrequency] = useState<string[]>([])
+
+  const today = new Date()
+
+  // 요일 선택
+  const handleFrequencyChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const day = e.target.value
+    if (e.target.checked) {
+      setFrequency((prev) => [...prev, day])
+    } else {
+      setFrequency((prev) => prev.filter((item) => item !== day))
+    }
+  }
+
+  const handleSubmit = async () => {
+    const newHabit: Habit = {
+      name: habitName,
+      startDate: startDate?.toISOString().split('T')[0] || '',
+      endDate: endDate?.toISOString().split('T')[0] || '',
+      frequency,
+      completedDates: [],
+    }
+
+    try {
+      await addDoc(collection(db, 'habits'), newHabit)
+      onAddHabit()
+      alert('루틴 등록 완료!')
+      onClose()
+    } catch (e) {
+      console.error('루틴 추가 실패: ', e)
+      alert('오류가 발생했습니다. 다시 시도해주세요.')
+    }
+  }
+
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50"
+      onClick={onClose}
+    >
+      <div
+        className="mx-auto max-w-xl rounded-lg bg-white p-4 shadow-md"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <h2 className="mb-6 text-2xl font-semibold">🔥루틴 등록하기</h2>
+
+        <div className="mb-4">
+          <label className="mb-2 block text-sm">루틴 이름</label>
+          <input
+            type="text"
+            value={habitName}
+            onChange={(e) => setHabitName(e.target.value)}
+            placeholder="등록할 루틴명을 입력하세요"
+            className="w-full rounded-full border-2 border-gray-300 px-4 py-2 outline-none focus:border-green-40"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="mb-2 flex items-center text-sm">시작 날짜</label>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            minDate={today} // 오늘 날짜 이후로 설정
+            className="w-full cursor-pointer rounded-full border-2 border-gray-300 px-4 py-2 outline-none focus:border-green-40"
+            dateFormat="yyyy-MM-dd"
+            placeholderText="날짜를 선택해주세요"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="mb-2 flex items-center text-sm">종료 날짜</label>
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            minDate={startDate || today} // 시작 날짜 이후로 설정
+            className="w-full cursor-pointer rounded-full border-2 border-gray-300 px-4 py-2 outline-none focus:border-green-40"
+            dateFormat="yyyy-MM-dd"
+            placeholderText="날짜를 선택해주세요"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="mb-2 block text-sm">매주 수행할 요일</label>
+          <div className="flex gap-4">
+            {['월', '화', '수', '목', '금', '토', '일'].map((day) => (
+              <label key={day} className="flex cursor-pointer items-center">
+                <input
+                  type="checkbox"
+                  value={day}
+                  checked={frequency.includes(day)}
+                  onChange={handleFrequencyChange}
+                  className="hidden"
+                />
+                <div
+                  className={`flex h-10 w-10 items-center justify-center rounded-full border-2 ${
+                    frequency.includes(day)
+                      ? 'border-green-20 bg-green-20'
+                      : 'border-gray-300'
+                  }`}
+                >
+                  <span
+                    className={`text-lg ${frequency.includes(day) ? 'text-white' : 'text-black'}`}
+                  >
+                    {day}
+                  </span>
+                </div>
+              </label>
+            ))}
+          </div>
+        </div>
+
+        <button
+          className="w-full rounded-full bg-green-30 px-6 py-3 text-white shadow-lg transition hover:bg-green-40"
+          onClick={handleSubmit}
+        >
+          루틴 추가
+        </button>
+      </div>
+    </div>
+  )
+}
