221b20dce95c222357c86145b597b81fcbb101ea
김보미 <kimbomi2172@naver.com>
2025-01-23 16:49:08 +0900

    💄 style: 친구 목록 디자인 수정

--- app/_components/Friends/FriendsList.tsx
+++ app/_components/Friends/FriendsList.tsx
@@ -17,7 +17,7 @@ export default function FriendList({
             key={friend.id}
             className="flex items-center justify-between rounded-lg bg-gray-100 p-4 shadow-md"
           >
-            <div>
+            <div className="flex flex-col md:flex-row md:items-center md:justify-center md:gap-4">
               <h3 className="text-lg font-semibold">{friend.nickname}</h3>
               <p className="text-sm text-gray-500">{friend.bio}</p>
             </div>
