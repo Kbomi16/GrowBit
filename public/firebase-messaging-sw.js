baba4237c822b35c6bd130e4edeeff3e67468c99
BORI <88658551+Kbomi16@users.noreply.github.com>
2025-01-26 14:50:51 +0900

    ♻️ refactor: 알림 설정 (#15)

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,11 +18,13 @@ firebase.initializeApp(firebaseConfig)
 const messaging = firebase.messaging()
 
 messaging.onBackgroundMessage((payload) => {
+  console.log('백그라운드 메시지 수신:', payload)
   const title = payload.notification?.title || '알림'
   const body = payload.notification?.body || '새로운 알림이 도착했습니다.'
   const options = {
     body: body,
     icon: '/icons/icon_maskable.png',
+    tag: payload.messageId, // messageId를 tag로 사용하여 중복을 방지
   }
 
   self.registration.showNotification(title, options)
